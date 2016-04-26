c631e775633e7e433e6523aa635d938386cc31de
Yuri Mikhaylov <me@yurijmi.ru>
2016-04-27 01:07:49 +0300

    Add step1.sh
    Signed-off-by: Yuri Mikhaylov <me@yurijmi.ru>

---
+++ step1.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+echo "Welcome to Deploy Rails!\n"
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+read -p "Your app's name in deploy.rb: " app_name
+
+read -p "Account for deploy (deploy): " deploy_user
+deploy_user=${deploy_user:-deploy}
+
+read -p "That account's password: " deploy_password
+
+read -p "Your public SSH key for login: " ssh_key
+
+echo "Installing software..."
+apt-get install sudo git-core curl wget nginx postgresql postgresql-server-dev-9.4 imagemagick nodejs gawk g++ gcc make libc6-dev libreadline6-dev zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 autoconf libgmp-dev libgdbm-dev libncurses5-dev automake libtool bison pkg-config libffi-dev -y
+
+echo "Adding a deploy account..."
+
+useradd -m -p $(echo $deploy_password | openssl passwd -1 -stdin) $deploy_user
+adduser $deploy_user sudo
+
+echo "Installing ssh keys..."
+
+echo $ssh_key >> ~/.ssh/authorized_keys
+runas -l  $deploy_user -c 'echo $ssh_key >> ~/.ssh/authorized_keys'
+
+echo "Creating role and db in postgres..."
+
+sudo -u postgres -H -- psql -c "create user $app_name with password '$deploy_password'; create database ${app_name}_production owner $app_name;"
+
+echo "Creating directory for deploy..."
+
+mkdir /var/www/$app_name
+chown $deploy_user:$deploy_user /var/www/$app_name
+
+echo "Updating configs..."
+
+rm /etc/nginx/sites-available/default
+rm /etc/nginx/sites-enabled/default
+service nginx restart
+
+rm /etc/ssh/sshd_config
+wget https://raw.githubusercontent.com/yurijmi/deploy_rails/master/conf/sshd_config /etc/ssh/sshd_config
+service ssh restart
+
+rm /etc/postgresql/9.4/main/pg_hba.conf
+wget https://raw.githubusercontent.com/yurijmi/deploy_rails/master/conf/pg_hba.conf /etc/postgresql/9.4/main/pg_hba.conf
+service postgresql restart
+
+echo "Run this as $deploy_user: \curl -sSL https://raw.githubusercontent.com/yurijmi/deploy_rails/master/step2.sh | bash -s"
+
+exit
